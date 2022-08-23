c6003beeab51041a776fe6528ff040ff3e666f89
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-23 16:03:54 +0545

    added .ensureInitialized()

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_project/app/view/app.dart';
 import 'app/flavours/app_flavour.dart';
 import 'core/service_locator.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   setUpServiceLocator();
   bootstrap(() => App());
 }
