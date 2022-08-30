9530aa331460c67a9c2de911a27e1c80f0d528bd
Utsav Ghimire <ghimireutsav11@gmail.com>
2022-08-31 02:22:07 +0545

    test for json serialization for UserModel

---
+++ test/features/authentication/models/user_model_test.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:flutter_project/features/auth/data/models/user_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../fixtures/fixture.dart';
+
+void main() {
+  final User user = User(firstName: 'Utsav', lastName: 'Ghimire');
+  group(
+    'json serialization ',
+    () {
+      test(
+        'fromJson should return UserModel object ',
+        () {
+          //arrange
+          final jsonData = fixture('test_response.json');
+          //act
+          final userModel = User.fromJson(jsonDecode(jsonData));
+          // assert
+          expect(userModel, user);
+        },
+      );
+      test(
+        'toJson should return json string ',
+        () {
+          //act
+          final jsonData = user.toJson();
+          // assert
+          final expextMap = {
+            'id': 0,
+            'username': '',
+            'password': '',
+            'email': '',
+            'firstName': 'Utsav',
+            'lastName': 'Ghimire',
+            'gender': '',
+            'image': '',
+            'token': ''
+          };
+          expect(expextMap, jsonData);
+        },
+      );
+    },
+  );
+}
